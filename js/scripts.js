dbce4d5d44407a9178bb33d12ad7e7c0e6506cf0
Vera weikel <vera.weikel@gmail.com>
2023-01-10 16:16:25 -0800

    It will add default/increased font size buttons to js.
    Co-authored-by: Asia Kaplanyan <a.kaplanyan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 window.onload = function(){
+  
+  // light/dark btns
   let buttonLight = document.querySelector("button.button-light")
   buttonLight.addEventListener('click', function(){
     document.body.style.backgroundColor = "white";
@@ -10,5 +12,19 @@ window.onload = function(){
     document.body.style.backgroundColor = "black";
     document.body.style.color = "white"
   });
+
+  // default/increasted font buttons
+  let buttonDefault = document.querySelector("button.button-default");
+  buttonDefault.addEventListener("click", function(){
+    document.body.style.fontSize = "12px";
+  });
   
+
+  let buttonIncreased = document.querySelector("button.button-increased");
+  buttonIncreased.addEventListener('click', function() {
+    document.body.style.fontSize = "40px";
+  });
+
+
+  // onmouseover and onmouseout event handlers 
 }
